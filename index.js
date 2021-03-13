baf4000a2eecb9b52d7f0d614053338667ea6aa7
LD4ank <mcc262005@gmail.com>
2021-03-13 15:02:03 -0600

    cambio de estado

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const config = require ("./config.json");
 
 client.on("ready", () => {
     console.log(`${client.user.username} Estoy listo amor`);
-    client.user.setActivity(" | TRAPITOS TRAPITOS TRAPITOS |")
+    client.user.setActivity(" |m!help|")
 });
 
 client.login('ODE3MTIzMzk0MzUyNzA5Njcz.YEE7dg.s-SFXUVDo2RVhbId2Y1C1AlYFW0');
